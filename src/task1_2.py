c68805176c49a17ed401d8917361e0e48d811639
fairwell-coding <coding@temmel.me>
2022-05-21 18:47:00 +0200

    task 1.2 finished

--- src/task1_2.py
+++ src/task1_2.py
@@ -10,20 +10,34 @@ if __name__ == '__main__':
   for idx in [1, 2, 3]:
     X_train, X_test, y_train, y_test = get_toy_dataset(idx)
     knn = KNearestNeighborsClassifier()
-    #TODO: use the `GridSearchCV` meta-classifier and search over different values of `k`!
-    # include the `return_train_score=True` option to get the training accuracies
-    clf = ...
+
+    param_grid = {
+        "k": range(1, 101)
+    }
+
+    clf = GridSearchCV(knn, param_grid, n_jobs=-1, return_train_score=True)
+    clf.fit(X_train, y_train)
+
     test_score = clf.score(X_test, y_test)
+
     print(f"Test Score: {test_score}")
     print(f"Dataset {idx}: {clf.best_params_}")
 
+    # plot dataset with decision boundary
     plt.figure()
+    plt.title(f'Dataset {idx} with decision boundary')
+    plt.xlabel('x1')
+    plt.ylabel('x2')
     plotting.plot_decision_boundary(X_train, clf)
     plotting.plot_dataset(X_train, X_test, y_train, y_test)
-    # TODO you should use the plt.savefig(...) function to store your plots before calling plt.show()
-    plt.show()
-    plt.figure()
-    plt.plot(clf.cv_results_['mean_train_score'])
-    plt.plot(clf.cv_results_['mean_test_score'])
     plt.show()
 
+    # plot training and validation score for all values of k
+    plt.figure()
+    plt.title(f'Dataset {idx}: training and validation scores')
+    plt.xlabel('k')
+    plt.ylabel('accuracy')
+    plt.plot(clf.cv_results_['mean_train_score'], label='mean training scores')
+    plt.plot(clf.cv_results_['mean_test_score'], label='mean validation scores')
+    plt.legend()
+    plt.show()
