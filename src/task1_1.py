d9ba3efc8a647c843d1b868410f1e75ab06c0c55
fairwell-coding <coding@temmel.me>
2022-05-21 16:05:05 +0200

    first working version of task 1.1

--- src/task1_1.py
+++ src/task1_1.py
@@ -7,18 +7,25 @@ class KNearestNeighborsClassifier(BaseEstimator):
     self.k = k
 
   def fit(self, X, y):
-    # TODO IMPLEMENT ME
-    # store X and y
-    return
+    self.X_train = X
+    self.y_train = y
+
+    return self
 
   def score(self, X, y):
     y_pred = self.predict(X)
     return np.mean(y_pred == y)
 
   def predict(self, X):
-    # TODO: assign class labels
-    # useful numpy methods: np.argsort, np.unique, np.argmax, np.count_nonzero
-    # pay close attention to the `axis` parameter of these methods
-    # broadcasting is really useful for this task!
-    # See https://numpy.org/doc/stable/user/basics.broadcasting.html
-    return
+    predictions = []
+
+    for i in range(X.shape[0]):
+      eucledian_distances = np.linalg.norm(self.X_train - X[i].reshape(-1, X[i].shape[0]), axis=1)
+      sorted_indices = np.argsort(eucledian_distances)[:self.k]  # first k indices of eucledian distances between all training samples and current test sample in ascending order
+      closest_classes = self.y_train[sorted_indices]
+
+      num_class_1 = np.count_nonzero(closest_classes)
+      num_class_0 = closest_classes.shape[0] - num_class_1
+      predictions.append(np.argmax([num_class_0, num_class_1]))
+
+    return predictions
