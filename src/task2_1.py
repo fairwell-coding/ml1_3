b0bba5365bb7f8dc051a6fb90feadfc40b241b78
fairwell-coding <coding@temmel.me>
2022-05-22 21:07:49 +0200

    task 2.1 finished, 2.2 implemented without argumentation

--- src/task2_1.py
+++ src/task2_1.py
@@ -7,16 +7,22 @@ from datasets import get_toy_dataset
 
 
 def loss(w, b, C, X, y):
-  # TODO: implement the loss function (eq. 1)
-  # useful methods: np.sum, np.clip
-  return np.inf
+  return 1/2 * w.T @ w + C * np.sum(np.clip(1 - y * (X @ w + b), a_min=0, a_max=None))
 
 
 def grad(w, b, C, X, y):
-  # TODO: implement the gradients with respect to w and b.
-  # useful methods: np.sum, np.where, numpy broadcasting
-  grad_w = ...
-  grad_b = ...
+  # define condition for piece-wise gradient
+  grad_condition = 1 - y * (X @ w + b)  # piece-wise condition for gradient being >= 0
+  X_grad = np.where(np.repeat(grad_condition.reshape((X.shape[0], 1)) >= 0, 2, axis=1), X, 0)
+
+  # calculate grad for w
+  inner_exp = -X_grad * y.reshape((X.shape[0], 1))
+  grad_w = w + C * np.sum(inner_exp, axis=0)
+
+  # calculate grad for b
+  y_grad = np.where(grad_condition >= 0, y, 0)
+  grad_b = C * np.sum(-y_grad)
+
   return grad_w, grad_b
 
 
@@ -26,31 +32,41 @@ class LinearSVM(BaseEstimator):
     self.C = C
     self.max_iter = max_iter
     self.eta = eta
+    self.tolerance = self.eta
 
   def fit(self, X, y):
     # TODO: initialize w and b. Does the initialization matter?
     # convert y: {0,1} -> -1, 1
     y = np.where(y == 0, -1, 1)
-    self.w = np.random.normal(size=X.shape[1])
+    self.w = np.random.normal(loc=0, scale=1.0, size=X.shape[1])
     self.b = 0.
+    # self.w = [6.87296361, 9.56862135]
+    # self.b = -10
     loss_list = []
 
     for j in range(self.max_iter):
-      # TODO: compute the gradients, update the weights, compute the loss
       grad_w, grad_b = grad(self.w, self.b, self.C, X, y)
-      self.w = ...
-      self.b = ...
+      self.w = self.w - self.eta * grad_w
+      self.b = self.b - self.eta * grad_b
       loss_list.append(loss(self.w, self.b, self.C, X, y))
+
+      if len(loss_list) >= 2 and np.isclose(loss_list[-2], loss_list[-1], rtol=self.tolerance):
+        break
+
+      # if len(loss_list) >= 6 and np.isclose(loss_list[-6:-1], loss_list[-1], rtol=self.tolerance):
+      #   break
+
+    print(f'Model fit: C = {self.C}, eta = {self.eta}, tolerance = {self.tolerance}, num_epochs = {len(loss_list)}')
 
     return loss_list
 
   def predict(self, X):
-    # TODO: assign class labels to unseen data
-    y_pred = ...
+    y_regressed = X @ self.w + self.b  # predition regression value of model
+    y_pred = np.where(np.tanh(y_regressed) > 0, 1, -1)  # decide on binary class label based on regressed value
+
     # converting y_pred from {-1, 1} to {0, 1}
     return np.where(y_pred == -1, 0, 1)
 
   def score(self, X, y):
-    # TODO: IMPLEMENT ME
     y_pred = self.predict(X)
     return np.mean(y_pred == y)
--- src/task2_2.py
+++ src/task2_2.py
@@ -1,24 +1,77 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 import plotting
 from datasets import get_toy_dataset
 from task2_1 import LinearSVM
 from sklearn.model_selection import GridSearchCV
 
-if __name__ == '__main__':
-  X_train, X_test, y_train, y_test = get_toy_dataset(1, remove_outlier=True)
-  svm = LinearSVM()
-  # TODO use grid search to find suitable parameters!
-  clf = ...
-  clf.fit(X_train, y_train)
 
-  # TODO Use the parameters you have found to instantiate a LinearSVM.
-  # the `fit` method returns a list of scores that you should plot in order
-  # to monitor the convergence. When does the classifier converge?
-  svm = LinearSVM(...)
+RANDOM_SEED = 28  # 33, 28
+
+
+def __find_optimal_parameters_using_gridsearch():
+  """ Use gridsearch to find optimal parameters for C and eta.
+  """
+
+  param_grid = {
+    "C": 10. ** np.arange(-5, 6),
+    "eta": np.logspace(-1, -6, 6)
+  }
+  base_estimator = LinearSVM()
+  grid_search = GridSearchCV(base_estimator, param_grid, n_jobs=-1)
+  grid_search.fit(X_train, y_train)
+
+  scores = grid_search.cv_results_
+  best_params = grid_search.best_params_
+  best_score = grid_search.best_score_
+
+  print(f'The best SVM score is {best_score}.')
+  print(f'The best SVM parameters found by GridSearchCV are {best_params}.')
+
+  return best_params["C"], best_params["eta"]
+
+
+def __use_best_parameters_to_instantiate_linear_svm(C, eta):
+  """ Use best parameters found by GridSearch to instantiate a LinearSVM.
+  """
+
+  svm = LinearSVM(C=C, eta=eta)
   scores = svm.fit(X_train, y_train)
+
+  plt.figure()
+  plt.title("Training loss curve")
+  plt.xlabel('epochs/iterations')
+  plt.ylabel('training loss')
   plt.plot(scores)
-  test_score = clf.score(X_test, y_test)
+
+  plt.figure()
+  plt.title("Training loss curve (y-axis clipped)")
+  plt.xlabel('epochs/iterations')
+  plt.ylabel('training loss')
+  plt.ylim(0, 200)
+  plt.plot(scores)
+
+  test_score = svm.score(X_test, y_test)
   print(f"Test Score: {test_score}")
 
-  # TODO plot the decision boundary!
+  return svm
+
+
+def __plot_dataset_with_decision_boundary(svm):
+  plt.figure()
+  plt.title(f'Dataset 1 (outlier removed) with decision boundary')
+  plt.xlabel('x1')
+  plt.ylabel('x2')
+  plotting.plot_decision_boundary(X_train, svm)
+  plotting.plot_dataset(X_train, X_test, y_train, y_test)
+  plt.show()
+
+
+if __name__ == '__main__':
+  np.random.seed(RANDOM_SEED)
+  X_train, X_test, y_train, y_test = get_toy_dataset(1, remove_outlier=True)
+
+  # C, eta = __find_optimal_parameters_using_gridsearch()
+  svm = __use_best_parameters_to_instantiate_linear_svm(100, 1e-3)
+  __plot_dataset_with_decision_boundary(svm)
